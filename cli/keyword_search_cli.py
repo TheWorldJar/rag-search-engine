0dfca2e5d61c7c8eca0e4a6d141f9996af765c25
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2026-01-08 18:46:07 -0500

    Add NLTK dependency and enhance keyword search functionality with tokenization, stopword removal, and stemming

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,15 +2,51 @@
 
 import argparse
 import json
+import re
+import string
+from typing import TypedDict, cast
+
+from nltk.stem import PorterStemmer
+
+
+class Movie(TypedDict):
+    id: int
+    title: str
+    description: str
+
+
+def tokenize(query: str) -> list[str]:
+    translator = str.maketrans("", "", string.punctuation)
+    return query.translate(translator).lower().split(" ")
+
+
+def stop(query: list[str]) -> list[str]:
+    f = open("./data/stopwords.txt", "r", encoding="utf-8")
+    stop_words = f.read().splitlines()
+    f.close()
+    return [token for token in query if token not in stop_words]
+
+
+def stem(query: list[str]) -> list[str]:
+    stemmer = PorterStemmer()
+    return [stemmer.stem(token) for token in query]
 
 
 def keywordSearch(query: str) -> None:
-    found = []
+    found: list[str] = []
+    translator = str.maketrans("", "", string.punctuation)
+    stemmed_query = stem(stop(tokenize(query)))
+
     with open("./data/movies.json", "r", encoding="utf-8") as f:
-        movies = json.load(f)
-        for movie in movies["movies"]:
-            if query in movie["title"]:
-                found.append(movie["title"])
+        movies: list[Movie] = cast(list[Movie], json.load(f)["movies"])
+        for movie in movies:
+            translated_movie_title = movie["title"].translate(translator).lower()
+            for q_token in stemmed_query:
+                pattern = re.compile(rf"{re.escape(q_token)}.*")
+                if pattern.search(translated_movie_title):
+                    found.append(movie["title"])
+                    break
+        f.close()
 
     for i in range(len(found)):
         print(f"{i}. {found[i]}")
@@ -21,14 +57,14 @@ def main() -> None:
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
-    search_parser.add_argument("query", type=str, help="Search query")
+    _ = search_parser.add_argument("query", type=str, help="Search query")
 
     args = parser.parse_args()
 
-    match args.command:
+    match cast(str, args.command).lower():
         case "search":
-            print("Searching for:", args.query)
-            keywordSearch(args.query)
+            print("Searching for:", cast(str, args.query))
+            keywordSearch(cast(str, args.query))
         case _:
             parser.print_help()
 
